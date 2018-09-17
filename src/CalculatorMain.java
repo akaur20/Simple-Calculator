c7fdc356f8851313efbf28a99b113007dcd2ffbf
wizleen <wizleen@AVLEEN>
2018-09-17 18:13:06 -0400

    New Calculator

--- src/CalculatorMain.java
+++ src/CalculatorMain.java
@@ -5,7 +5,9 @@ public class CalculatorMain
 	public static void main(String[] args) 
 	{
 		Calculator calc = new Calculator();
-		System.out.print(calc.add(2, 4));
+		System.out.println(calc.add(2, 4));
+		System.out.println(calc.subtract(10,6));
+		
 	}
 
 }
